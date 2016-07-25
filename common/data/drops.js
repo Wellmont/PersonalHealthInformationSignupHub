f72f29f38d482af0fac67f977a8629e195cdd19e
PokemonGoDB <NeeleyDan@gmail.com>
2016-07-25 15:12:53 -0400

    Update drops.js

--- common/data/drops.js
+++ common/data/drops.js
@@ -11,7 +11,7 @@ window.drops = {
 		{
             "name": "Wetlands",
             "thumb": 28,
-            "Areas classified as Wetlands may have higher counts of Water Pokemon": [ 7, 8, 9, 54, 55, 60, 61, 62, 72, 73, 79, 80, 86, 87, 90, 91, 98, 99, 116, 117, 118, 119, 120, 121, 129, 130, 131, 134, 138, 139, 140, 141 ],
+            "Areas classified as Wetlands may have higher counts of this type of Pokemon": [ 7, 8, 9, 54, 55, 60, 61, 62, 79, 80, 86, 87, 90, 91, 98, 99, 116, 117, 118, 119, 120, 121, 129, 130, 131, 134, 138, 139, 140, 141, 1, 2, 3, 13, 14, 15, 23, 24, 29, 30, 31, 32, 33, 34, 41, 42, 43, 44, 45, 48, 49, 69, 70, 71, 72, 73, 88, 89, 92, 93, 94, 109, 110 ],
 		},
 		
 		{
@@ -23,7 +23,7 @@ window.drops = {
 		{
             "name": "Residential",
             "thumb": 28,
-            "Neighborhoods have a slightly higher Fire type population": [ 4, 5, 6, 37, 38, 58, 59, 77, 78, 126, 136 ],
+            "Neighborhoods have a slightly higher population of this Pokemon type": [ 4, 5, 6, 37, 38, 58, 59, 77, 78, 126, 136, 16, 17, 18, 19, 20, 21, 22, 39, 40, 52, 53, 83, 84, 85, 108, 113, 115, 128, 133, 137, 143 ],
 		},
 		{
             "name": "Cities",
@@ -40,49 +40,88 @@ window.drops = {
 		{
             "name": "Parks",
             "thumb": 28,
-            "Parks are a great location to find Grass types": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114 ],
+            "Parks are a great location to find this Pokemon type": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114, 10, 11, 12, 13, 14, 15, 48, 49, 123, 127 ],
 		},
 		
 		
 		{
             "name": "Golf Courses",
             "thumb": 28,
-            "Or any area with large plots of grass": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114 ],
+            "Or any area with large plots of grass": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114, 10, 11, 12, 13, 14, 15, 48, 49, 123, 127 ],
 		},
 		
 		
 			{
             "name": "Nature",
             "thumb": 28,
-            "Hiking Trails and other reserves have higher Grass type encounters": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114 ],
+            "Hiking Trails and other reserves have higher encounter rates for this type": [ 1, 2, 3, 43, 44, 45, 46, 47, 69, 70, 71, 102, 103, 114, 10, 11, 12, 13, 14, 15, 48, 49, 123, 127 ],
 		},
 		
 				{
-            "name": "",
+            "name": "Cities",
             "thumb": 28,
-            "": [  ],
+            "Downtown areas have a slightly higher Electric type population": [ 25, 26, 81, 82, 100, 101, 125, 135 ],
 		},
 				{
-            "name": "",
+            "name": "College Campuses",
             "thumb": 28,
-            "": [  ],
+            "University and College campuses have high populations of this Pokemon type": [ 25, 26, 81, 82, 100, 101, 125, 135, 16, 17, 18, 19, 20, 21, 22, 39, 40, 52, 53, 83, 84, 85, 108, 113, 115, 128, 133, 137, 143 ],
 		},
 				{
-            "name": "",
+            "name": "Nature",
             "thumb": 28,
-            "": [  ],
+            "Hiking Trails and other reserves have higher populations of this type": [ 74, 75, 76, 95, 111, 112, 138, 139, 140, 141, 142, 27, 28, 31, 34, 50, 51, 104, 105 ],
 		},
 				{
-            "name": "",
+            "name": "Farm Land",
             "thumb": 28,
-            "": [  ],
+            "Farm land has a higher population of this Pokemon type": [ 74, 75, 76, 95, 111, 112, 138, 139, 140, 141, 142, 27, 28, 31, 34, 50, 51, 104, 105 ],
 		},
 				{
-            "name": "",
+            "name": "Residential (Night)",
             "thumb": 28,
-            "": [  ],
+            "Neighborhoods at Night have a slightly higher Psychic type population": [ 63, 64, 65, 79, 80, 96, 97, 102, 103, 121, 122, 124 ],
 		},
-		
+				{
+            "name": "Hospitals",
+            "thumb": 28,
+            "Note: Always be respectful when hunting near Hospitals": [ 63, 64, 65, 79, 80, 96, 97, 102, 103, 121, 122, 124 ],
+		},
+						{
+            "name": "Industrial",
+            "thumb": 28,
+            "Manufacturing and Chemical plants have higher encounter rates for this type": [ 1, 2, 3, 13, 14, 15, 23, 24, 29, 30, 31, 32, 33, 34, 41, 42, 43, 44, 45, 48, 49, 69, 70, 71, 72, 73, 88, 89, 92, 93, 94, 109, 110 ],
+		},
+						{
+            "name": "Landmarks",
+            "thumb": 28,
+            "Major landmarks are a real draw for this Pokemon type": [ 147, 148, 149, 35, 36, 39, 40, 122 ],
+		},
+						{
+            "name": "Beaches",
+            "thumb": 28,
+            "This Pokemon type appears to like the Beach!": [ 35, 36, 39, 40, 122 ],
+		},
+						{
+            "name": "Gyms",
+            "thumb": 28,
+            "Fighting types love to get swole at the Gym": [ 56, 57, 62, 66, 67, 68, 106, 107 ],
+		},
+						{
+            "name": "Stadiums",
+            "thumb": 28,
+            "They also love to compete at Sports Stadiums": [ 56, 57, 62, 66, 67, 68, 106, 107 ],
+		},
+						{
+            "name": "Night Time",
+            "thumb": 28,
+            "Ghosts like to come out at Night in lots of locations": [ 92, 93, 94 ],
+		},
+						{
+            "name": "Water",
+            "thumb": 28,
+            "Look for Ice types in large bodies of water (be careful!)": [ 124 ],
+		},				
 				
   ]
 
